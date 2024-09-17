751ff5420149c8c2effb699cf862bc2802a64f21
main73 <mainuddinrusselbuet73@yahoo.com>
2024-09-17 14:52:20 +0600

    Updates

---
+++ precache-manifest.8dffa9cd560e16b79da4b57f21efadd6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b228cb3721d8de7bf51409452a571364",
+    "url": "/tailwind-css-food/index.html"
+  },
+  {
+    "revision": "e8dd57a50c679fc0c9ad",
+    "url": "/tailwind-css-food/static/css/2.7b9725fc.chunk.css"
+  },
+  {
+    "revision": "d2c82d49c8fe425127b4",
+    "url": "/tailwind-css-food/static/css/main.ed989875.chunk.css"
+  },
+  {
+    "revision": "e8dd57a50c679fc0c9ad",
+    "url": "/tailwind-css-food/static/js/2.4c1cad17.chunk.js"
+  },
+  {
+    "revision": "3232a3baa9a05f3e2b89",
+    "url": "/tailwind-css-food/static/js/3.9973b74a.chunk.js"
+  },
+  {
+    "revision": "d2c82d49c8fe425127b4",
+    "url": "/tailwind-css-food/static/js/main.c6400e3a.chunk.js"
+  },
+  {
+    "revision": "f4d091f99608fb4b4003",
+    "url": "/tailwind-css-food/static/js/runtime~main.abdeff0f.js"
+  },
+  {
+    "revision": "f9e15fe94364c4421c72a1cc0dd774b7",
+    "url": "/tailwind-css-food/static/media/banner1.f9e15fe9.png"
+  },
+  {
+    "revision": "723c0b9413e5c7a242d3dd27d636ddb7",
+    "url": "/tailwind-css-food/static/media/banner2.723c0b94.png"
+  },
+  {
+    "revision": "20f85a219f86f93d85168a31defa870f",
+    "url": "/tailwind-css-food/static/media/banner3.20f85a21.png"
+  },
+  {
+    "revision": "f6dcdcba61497a627a74f8fe8db2ddb2",
+    "url": "/tailwind-css-food/static/media/foodpanda.f6dcdcba.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tailwind-css-food/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tailwind-css-food/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tailwind-css-food/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tailwind-css-food/static/media/slick.f97e3bbf.svg"
+  }
+]);
